f25dce7d937ed45301701f5c86b2a5a2a62e7423
Carlos Andres Diaz Pinto <andresvis565@gmail.com>
2024-05-09 02:36:09 -0500

    Tema 3
    Se maqueta tema 3 con pendientes de diseño por validar

--- src/views/Tema3.vue
+++ src/views/Tema3.vue
@@ -6,6 +6,111 @@
       .titulo-principal__numero
         span 3
       h1 Medición de indicadores
+    
+    p En la búsqueda de soluciones para las problemáticas sociales que sufre la humanidad diariamente como la pobreza, el hambre, la desigualdad, la degradación ambiental, la violencia, las injusticias, entre otras, las cuales paradójicamente se presentan en mayor proporción en las comunidades y la población que se encarga de la agricultura familiar y campesina, se han implementado estrategias erróneas que ha afectado y agudizado estas problemáticas.
+    p.mb-5 Por lo anterior, es necesario llevar a cabo la transformación de la agricultura y los sistemas alimentarios, la unión del conocimiento científico y los saberes locales, la minimización de los intermediarios en la cadena de producción y comercialización, para así lograr aportar a mejorar las necesidades actuales y futuras.
+
+    .row.mb-5.bg_grad-07.position-relative
+      .col-lg-9.align-content-center
+        p.mb-0.px-5.py-3 Se deben realizar seguimientos por medio de indicadores que permiten comprender y llevar a cabo la evaluación de los agroecosistemas campesinos, es decir establecer que estos conserven y mantengan una producción estable en el tiempo, a pesar de las limitaciones tecnológicas, climáticas, ecológicas; respetando el entorno, la oferta ambiental y la base biológica, como también satisfaciendo el componente social. 
+      .col-lg-3
+        figure
+          img(src='@/assets/curso/temas/tema3/1.svg', alt='Texto que describa la imagen')
+      img.pst_20(src='@/assets/curso/temas/tema3/2.svg', alt='Texto que describa la imagen')
+    
+    .cajon.color-acento-contenido.bgr_5.mb-5
+      p.mb-0.px-5.py-4 Debido a que los indicadores son estadísticas que condensan el comportamiento (estado, evolución y dinámica) de variables de interés y que por medio de su análisis se puede llevar a cabo procesos de mejora; s importante, la adecuada obtención y procesamiento de la información, logrando obtener indicadores confiables, que sean fáciles de aplicar, que posibiliten incorporar cambios con el paso del tiempo, que sean fáciles de interpretar, universales, pero al mismo tiempo se puedan aplicar a casos particulares.
+  
+    .row.mb-5.bgr_13.p-4.justify-content-end.position-relative
+      .col-lg-10.align-content-center
+        p Como lo indica Domínguez, Blancas, Guerrero y González (2011), para la elaboración de los indicadores hay diferentes metodologías en función de clase de índice que se quiere obtener, existiendo métodos como:
+        div
+          ol.lista-ol
+            li 
+              span.text-bold 1. 
+              | Agregación de indicadores simples.
+            li 
+              span.text-bold 2. 
+              | Construcción participativa.
+            li 
+              span.text-bold 3. 
+              | Análisis multivariable. 
+            li 
+              span.text-bold 4. 
+              | Análisis de componentes principales.
+            li 
+              span.text-bold 5. 
+              | Análisis factorial.
+            li 
+              span.text-bold 6. 
+              | Escalamiento óptimo. 
+      .col-lg-2
+        figure
+          img(src='@/assets/curso/temas/tema3/4.svg', alt='Texto que describa la imagen')
+      img.pst_21(src='@/assets/curso/temas/tema3/5.svg', alt='Texto que describa la imagen')
+    
+    separador
+    
+    #t_3_1.titulo-segundo.color-acento-contenido
+      h2 3.1 Metodología MESMIS
+    
+    p.mb-5 La metodología del Marco para la evaluación de sistemas de manejo de recursos naturales incorporando indicadores de sustentabilidad (MESMIS) es una herramienta innovadora para hacer frente a los interrogantes planteados en el área de evaluaciones de sustentabilidad. Esta se basa en el enfoque participativo de quienes hacen parte del proceso evaluativo en los agroecosistemas, que cuenta con una estructura flexible que permite adaptarse a diferentes necesidades y ambientes.
+    
+    .cajon.color-secundario.p-4.bgr_4.mb-5 
+      p Sin embargo, la evaluación es específica y se realiza de forma personalizada, es decir, es para un sistema específico que esté en un lugar geográfico determinado o para una escala espacial y temporal definida. Esta sustentabilidad se evalúa de manera relativa o comparativa a través de la identificación de las debilidades y oportunidades en los ámbitos económico, social y ambiental. 
+      p.mb-0 Para aplicar la metodología, se propone un ciclo de evaluación que comprende elementos o pasos que se ven en la siguiente figura:
+
+    .titulo-sexto.color-acento-contenido
+      h5 Figura 3.
+      span Esquema general del MESMIS
+
+    figure.mb-5
+      img(src='@/assets/curso/temas/tema3/6.svg', alt='Texto que describa la imagen')
+
+    p.mb-5 Para lo cual es necesario la identificación de atributos que poseen los agroecosistemas, como, por ejemplo:
+
+    SlyderF.mb-5(columnas="col-lg-6 col-xl-3")
+      .tarjeta-avatar
+        div.bgr_6
+          figure
+            img.alto(src='@/assets/curso/temas/tema3/7.png', alt='Texto que describa la imagen')
+          p.text-center.text-bold.mb-0.p-3.bgr_7 Equidad
+      .tarjeta-avatar
+        div.bgr_6
+          figure
+            img.alto(src='@/assets/curso/temas/tema3/8.png', alt='Texto que describa la imagen')
+          p.text-center.text-bold.mb-0.p-3.bgr_7 Adaptabilidad
+      .tarjeta-avatar
+        div.bgr_6
+          figure
+            img.alto(src='@/assets/curso/temas/tema3/9.png', alt='Texto que describa la imagen')
+          p.text-center.text-bold.mb-0.p-3.bgr_7 Estabilidad
+      .tarjeta-avatar
+        div.bgr_6
+          figure
+            img.alto(src='@/assets/curso/temas/tema3/10.png', alt='Texto que describa la imagen')
+          p.text-center.text-bold.mb-0.p-3.bgr_7 Productividad
+      .tarjeta-avatar
+        div.bgr_6
+          figure
+            img.alto(src='@/assets/curso/temas/tema3/11.png', alt='Texto que describa la imagen')
+          p.text-center.text-bold.mb-0.p-3.bgr_7 Resiliencia
+    
+    p.mb-5 Para llevar a cabo un ciclo de evaluación del agroecosistema a través de la metodología MESMIS, se realiza a través de 6 pasos que requieren procesos como la identificación de los atributos, la definición de puntos críticos, la identificación de criterios de diagnóstico, la construcción de indicadores estratégicos en el ámbito social, ambiental y económico.
+
+    .titulo-sexto.color-acento-contenido
+      h5 Figura 4.
+      span Ciclo de evaluación del MESMIS
+
+    figure.mb-5
+      img(src='@/assets/curso/temas/tema3/11.svg', alt='Texto que describa la imagen')
+
+    separador
+    
+    #t_3_2.titulo-segundo.color-acento-contenido
+      h2 3.2 Evaluación de la sustentabilidad mediante MESMIS
+    
+    p.mb-5 Al realizar el proceso metodológico para la implementación de la evaluación de la sustentabilidad de agroecosistemas a través del MESMIS, se debe iniciar seleccionando y caracterizando el agroecosistema (finca, parcelas, unidad productiva, comunidad, entre otros.) por medio de instrumentos como la encuesta, entrevistas, visitas y observaciones que permitan obtener información con aspectos biofísicos, socioeconómicos, tecnológicos, biodiversidad, calidad de vida y unión familiar, para luego mediante la obtención y procesamientos de la información estructurar los indicadores. En la siguiente figura se puede ver un ejemplo de indicadores.
 
 </template>
 
